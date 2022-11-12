6a2fa67c8060432b81e1325fab5f25f0412583ac
Franco Lorca <lorcafrancog@gmail.com>
2022-11-12 15:40:08 -0300

    fix
    minor fixes

--- front-end/js/register-form.js
+++ front-end/js/register-form.js
@@ -1,11 +1,22 @@
-const $btn = document.getElementById("btn-register")
+const $btn = document.getElementById("btn-register");
+const $nameInput = document.getElementById("name");
+const $emailInput = document.getElementById("mail");
+const $passwordInput = document.getElementById("password");
+const $passwordRepeatInput = document.getElementById("password_repeat");
+const $telInput = document.getElementById("tel");
 
 document.addEventListener("submit", (e) => {
-  e.preventDefault()
-})
+  e.preventDefault();
+});
 
 document.addEventListener("input", (e) => {
-  if (e.target.reportValidity()) {
+  if (
+    $nameInput.validity.valid  &&
+    $emailInput.validity.valid  &&
+    $passwordInput.validity.valid  &&
+    $passwordRepeatInput.validity.valid  &&
+    $telInput.validity.valid 
+  ) {
     $btn.setAttribute("data-bs-toggle", "modal");
   }
-})
+});
