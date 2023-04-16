8d0521bc10174e03e4da9a38ce580ed296bcbae7
ptarrago <p.tarrago97@gmail.com>
2023-04-16 11:29:05 -0300

    Agrego la pagina de registro y el routing needed

--- frontend/destino-cordoba/src/app/components/navbar/navbar.component.ts
+++ frontend/destino-cordoba/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  irAFormulario() {
+    this.router.navigate(['/formulario']);
+  }
 }
