3cb28bab7eec0e01d37ddad3789baaf52ae2ff99
federico42o <fedes7777@gmail.com>
2023-04-15 22:39:06 -0300

    base html:navbar y footer

--- frontend/destino-cordoba/src/app/app.module.ts
+++ frontend/destino-cordoba/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
