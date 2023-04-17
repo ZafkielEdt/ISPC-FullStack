d0ed06dffa9d4868d3969853a6bce1c3a5c665b4
federico42o <fedes7777@gmail.com>
2023-04-16 22:30:13 -0300

    stepper

--- frontend/destino-cordoba/src/app/components/shared/shared.module.ts
+++ frontend/destino-cordoba/src/app/components/shared/shared.module.ts
@@ -1,17 +1,63 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
     MatGridListModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule
+    
+
   ],
   exports: [
     MatGridListModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    FormsModule
     
   ]
 })
--- frontend/destino-cordoba/src/app/components/travel-package/travel-package.component.ts
+++ frontend/destino-cordoba/src/app/components/travel-package/travel-package.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { FormBuilder, Validators } from '@angular/forms';
 export interface PackageTravel {
   title: string;
   description: string;
@@ -8,6 +8,8 @@ export interface PackageTravel {
   gallery: string[];
   days: number;
   nights: number;
+  childs?: number;
+  adults?: number;
 }
 
 @Component({
@@ -15,32 +17,72 @@ export interface PackageTravel {
   templateUrl: './travel-package.component.html',
   styleUrls: ['./travel-package.component.css']
 })
-export class PackageTravelComponent implements OnInit{
-
-  packageTravel: PackageTravel =  {
+export class PackageTravelComponent implements OnInit {
+  
+  isChecked = true;
+  packageTravel: PackageTravel = {
     title: this.route.snapshot.params['title'],
     description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
 
     price: 1000,
     gallery: [
-      "./assets/img/imagen1.webp","./assets/img/imagen2.webp","./assets/img/imagen3.webp","./assets/img/imagen4.webp","./assets/img/imagen5.webp","asdasdasd","asdasdasd","asdasdasd"
+      "./assets/img/imagen1.webp", "./assets/img/imagen2.webp", "./assets/img/imagen3.webp", "./assets/img/imagen4.webp", "./assets/img/imagen5.webp", "asdasdasd", "asdasdasd", "asdasdasd"
     ],
-    days:7,
-    nights:6
-
-  };
-  galleryLength : boolean = this.packageTravel.gallery.length > 4;
-  totalDuration = this.packageTravel.days.toString()  + ' DÍAS / ' + this.packageTravel.nights.toString()+' NOCHES';
-  constructor(private route: ActivatedRoute) { }
-  
-  
-  
-  
-  ngOnInit(): void {
+    days: 7,
+    nights: 6,
+    adults:1,
+    childs:1
     
+  };
+  totalDuration = this.packageTravel.days.toString() + ' DÍAS / ' + this.packageTravel.nights.toString() + ' NOCHES';
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+
+  constructor(private route: ActivatedRoute, private _formBuilder: FormBuilder) { }
+  
+  selected: Date = new Date();
+  
+  get endDate(): Date {
+    if (this.selected) {
+      const endDate = new Date(this.selected);
+      endDate.setDate(endDate.getDate() + this.packageTravel.days - 1);
+      return endDate;
+    }
+    return new Date();
+  }
+
+
+  incrementValue(type:string) : void{
+    if(type === 'childs' && this.packageTravel.childs !== undefined){
+      this.packageTravel.childs >= 0 ? this.packageTravel.childs++ : this.packageTravel.childs = 0;
+    }
+    if(type === 'adults' && this.packageTravel.adults){
+      this.packageTravel.adults >= 0 ? this.packageTravel.adults++ : this.packageTravel.adults = 1;
+    }
+  }
+  decrementValue(type:string) : void{
+
+    if(type === 'childs' && this.packageTravel.childs !== undefined){
+      this.packageTravel.childs > 0 ? this.packageTravel.childs-- : this.packageTravel.childs = 0;
+    }
+    if(type === 'adults' && this.packageTravel.adults){
+      this.packageTravel.adults > 1 ? this.packageTravel.adults-- : this.packageTravel.adults = 1;
+    }
+    
+  }
+
+  ngOnInit(): void {
+
     this.route.params.subscribe(params => {
       const title = params['title'];
     });
   }
+
+
 
 }
