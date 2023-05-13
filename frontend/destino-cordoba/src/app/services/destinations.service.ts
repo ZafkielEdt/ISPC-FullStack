e1959f49921da91dca0c8240bae58a55e64bfd94
Franco Lorca <lorcafrancog@proton.me>
2023-05-12 21:33:28 -0300

    👔 update(destinations.service.ts): Import HttpClient and add get method

--- frontend/destino-cordoba/src/app/services/destinations.service.ts
+++ frontend/destino-cordoba/src/app/services/destinations.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DestinationsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private pathUrl: string = "http://localhost:3000/destinations"
+
+  getDestinations() {
+    return this.http.get(this.pathUrl+'destinations')
+  }
 }
