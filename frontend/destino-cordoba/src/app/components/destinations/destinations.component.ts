756453715c76a479bc85ec7ea4924906bcff76ab
Franco Lorca <lorcafrancog@proton.me>
2023-05-14 11:59:16 -0300

    👔 Update(destinations.component.ts): Implement get method from destinations.service

--- frontend/destino-cordoba/src/app/components/destinations/destinations.component.ts
+++ frontend/destino-cordoba/src/app/components/destinations/destinations.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import {
+  Destination,
+  DestinationsService,
+} from 'src/app/services/destinations.service';
 
 @Component({
   selector: 'app-destinations',
   templateUrl: './destinations.component.html',
-  styleUrls: ['./destinations.component.css']
+  styleUrls: ['./destinations.component.css'],
 })
 export class DestinationsComponent {
+  destination$: Destination[] = [];
 
+  constructor(private destinationsService: DestinationsService) {}
+
+  getDestinations() {
+    this.destinationsService.getDestinations().subscribe({
+      next: (data: Destination[]) => (this.destination$ = data),
+    });
+  }
 }
