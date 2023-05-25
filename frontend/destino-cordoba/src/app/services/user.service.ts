eb6fd9c3a2a1f670b89c3b4048ee0fabaf49e168
Franco Lorca <lorcafrancog@proton.me>
2023-05-25 15:38:29 -0300

    ✨ Feat: Add getBy and deleteBy methods

--- frontend/destino-cordoba/src/app/services/user.service.ts
+++ frontend/destino-cordoba/src/app/services/user.service.ts
@@ -21,4 +21,12 @@ export class UserService {
   getAll(): Observable<User[]> {
     return this.http.get<User[]>(this.userUrl);
   }
+
+  getBy(id: number): Observable<User> {
+    return this.http.get<User>(`${this.userUrl}/${id}`)
+  }
+
+  deleteBy(id: number) {
+    return this.http.delete<User>(`${this.userUrl}/${id}`)
+  }
 }
