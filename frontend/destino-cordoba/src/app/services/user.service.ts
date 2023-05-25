beccc7701d16b4d3eb41c60a278bcbc185fb642b
Franco Lorca <lorcafrancog@proton.me>
2023-05-25 12:08:01 -0300

    ✨ Feat: Create user service with getAll method

---
+++ frontend/destino-cordoba/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  private userUrl: string = 'http://localhost:3000/user';
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.userUrl);
+  }
+}
