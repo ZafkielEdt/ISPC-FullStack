bc1eaf8dccd20ad4cea13782ac9433c0ea9eae8e
Franco Lorca <lorcafrancog@proton.me>
2023-05-25 15:41:49 -0300

    📝 Update(user-tab.component.html, user-tab.component.ts): Add delete and show modal

--- frontend/destino-cordoba/src/app/components/admin-dashboard-components/user-tab/user-tab.component.ts
+++ frontend/destino-cordoba/src/app/components/admin-dashboard-components/user-tab/user-tab.component.ts
@@ -10,9 +10,19 @@ import { User, UserService } from 'src/app/services/user.service';
 export class UserTabComponent {
   users$: Observable<User[]>;
   windowSize: boolean = false;
+  update: boolean = false;
 
   constructor(private userService: UserService) {
     this.users$ = userService.getAll();
     this.windowSize = window.innerWidth > 768;
+  }
+
+  clickUpdate(){
+    this.update = !this.update;
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteBy(id).subscribe();
+    window.location.reload();
   }
 }
