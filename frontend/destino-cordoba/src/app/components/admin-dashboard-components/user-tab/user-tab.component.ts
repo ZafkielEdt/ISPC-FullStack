1835ad4b3f30dec99b8faa2f1150ff944fba72f1
Franco Lorca <lorcafrancog@proton.me>
2023-05-26 01:22:46 -0300

    👔 Update(user-tab.component.ts): Remove unused imports

--- frontend/destino-cordoba/src/app/components/admin-dashboard-components/user-tab/user-tab.component.ts
+++ frontend/destino-cordoba/src/app/components/admin-dashboard-components/user-tab/user-tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output } from '@angular/core';
+import { Component} from '@angular/core';
 import { Observable } from 'rxjs';
 import { User, UserService } from 'src/app/services/user.service';
 
