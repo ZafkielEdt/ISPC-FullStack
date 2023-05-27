29b077781a2579f503643ded0c5b2c56b9409e31
Franco Lorca <lorcafrancog@proton.me>
2023-05-27 13:42:39 -0300

    👔 Update(destinations.service.ts): Add post method

--- frontend/destino-cordoba/src/app/services/destinations.service.ts
+++ frontend/destino-cordoba/src/app/services/destinations.service.ts
@@ -30,6 +30,10 @@ export class DestinationsService {
     return this.http.get<Destination>(`${this.destinationUrl}/${id}`);
   }
 
+  post(destination: FormGroup) {
+    return this.http.post(this.destinationUrl, destination.value)
+  }
+
   update(destination: FormGroup, id: number) {
     return this.http.put(`${this.destinationUrl}/${id}`, destination.value);
   }
