a29c6c70055379c95055dadfb91ed850729cb92a
Franco Lorca <lorcafrancog@proton.me>
2023-05-27 13:43:12 -0300

    👔 Update(app.routing.module.ts): Add routing for new component

--- frontend/destino-cordoba/src/app/app-routing.module.ts
+++ frontend/destino-cordoba/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { DestinationUpdateDetailsComponent } from './components/admin-dashboard-components/destination-tab/destination-update-details/destination-update-details.component';
+import { DestinationCreationFormComponent } from './components/admin-dashboard-components/destination-tab/destination-creation-form/destination-creation-form.component';
 import { DestinationsComponent } from './components/destinations/destinations.component';
 import {
   CartComponent,
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'travel/:title', component: PackageTravelComponent },
   { path: 'user-dashboard', component: UserDashboardComponent, canActivate: [AuthGuard]},
   {path: 'user-dashboard/destinations/:destinationId', component: DestinationUpdateDetailsComponent},
+  {path: 'user-dashboard/destination/new-destination', component: DestinationCreationFormComponent},
   { path: 'cards', component: DestinationsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'search/:type/:destino', component: ResultadoBuscadorComponent },
