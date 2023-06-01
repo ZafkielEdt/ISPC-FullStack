0e635a270e785db61c183d55ab3b1a5c1910b93f
Franco Lorca <lorcafrancog@proton.me>
2023-06-01 15:51:53 -0300

    👔 Update(user-dashboard-container.component.html and .ts): Reestructure code

--- frontend/destino-cordoba/src/app/components/user-dashboard-components/user-dashboard-container/user-dashboard-container.component.ts
+++ frontend/destino-cordoba/src/app/components/user-dashboard-components/user-dashboard-container/user-dashboard-container.component.ts
@@ -1,26 +1,26 @@
-import { Component } from '@angular/core';
-import { LoginService } from 'src/app/auth/service/login.service';
-import { User } from 'src/app/models/user';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { LoginService } from "src/app/auth/service/login.service";
+import { User } from "src/app/models/user";
 
 @Component({
-  selector: 'app-user-dashboard-container',
-  templateUrl: './user-dashboard-container.component.html',
-  styleUrls: ['./user-dashboard-container.component.css']
+  selector: "app-user-dashboard-container",
+  templateUrl: "./user-dashboard-container.component.html",
+  styleUrls: ["./user-dashboard-container.component.css"],
 })
-export class UserDashboardContainerComponent {
-
+export class UserDashboardContainerComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
   currentUser!: User;
 
-  constructor(private authService: LoginService){
-    // this.currentUser = {
-    //   id: 1,
-    //   first_name: 'John',
-    //   last_name: 'Doe',
-    //   username: 'John',
-    //   email: 'john@gmail.com',
-    //   password: 'password',
-    //   rol: ['user']
-    // }
-    this.authService.getCurrentUser().subscribe(data => this.currentUser = data);
+  constructor(private authService: LoginService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService
+      .getCurrentUser()
+      .subscribe((data) => (this.currentUser = data));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
