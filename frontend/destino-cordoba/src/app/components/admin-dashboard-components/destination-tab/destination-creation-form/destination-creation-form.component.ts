b2d6cf43d556c7a85d1e414c33a68d38f4e9b2bf
Franco Lorca <lorcafrancog@proton.me>
2023-06-03 12:11:17 -0300

    👔 Update(destination-creation-form.component.ts): Change form submit function

--- frontend/destino-cordoba/src/app/components/admin-dashboard-components/destination-tab/destination-creation-form/destination-creation-form.component.ts
+++ frontend/destino-cordoba/src/app/components/admin-dashboard-components/destination-tab/destination-creation-form/destination-creation-form.component.ts
@@ -1,28 +1,46 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import { DestinationsService } from "src/app/services/destinations.service";
+import { CityService } from "src/app/services/city.service";
+import { City } from "src/app/models/city";
 
 @Component({
   selector: "app-destination-creation-form",
   templateUrl: "./destination-creation-form.component.html",
   styleUrls: ["./destination-creation-form.component.css"],
 })
-export class DestinationCreationFormComponent {
+export class DestinationCreationFormComponent implements OnInit {
+  cities!: City[];
+  city!: City;
+
   formData = this.formBuilder.group({
     name: "",
-    city: this.formBuilder.group({name: ''}),
+    city: "",
     description: "",
     image: "",
   });
 
   constructor(
     private formBuilder: FormBuilder,
-    private destinationService: DestinationsService
+    private destinationService: DestinationsService,
+    private cityService: CityService
   ) {}
 
   onSubmit() {
-    // this.destinationService.post(this.formData).subscribe((data) => {
-    //   "Data sended";
-    // });
+    const formDataForApi = {
+      name: this.formData.value.name || "",
+      city: this.cities.filter((c) => c.name === this.formData.value.city)[0],
+      description: this.formData.value.description || "",
+      images: [this.formData.value.image] || [],
+    };
+    this.destinationService.post(formDataForApi).subscribe((data) => {
+      console.log("Data sended");
+    });
+  }
+
+  ngOnInit(): void {
+    this.cityService.getAll().subscribe((res) => {
+      this.cities = res.results;
+    });
   }
 }
