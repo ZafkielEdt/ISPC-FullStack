e4374555feef70ea855910d33cec5ddd21294501
Franco Lorca <lorcafrancog@proton.me>
2023-06-08 17:07:08 -0300

    👔 Update(province.service.ts): Add update method

--- frontend/destino-cordoba/src/app/services/province.service.ts
+++ frontend/destino-cordoba/src/app/services/province.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {FormGroup} from "@angular/forms";
+import {Province} from "../models/province";
 
 @Injectable({
     providedIn: "root",
@@ -15,8 +16,16 @@ export class ProvinceService {
         return this.http.get<any>(this.provinceUrl);
     }
 
+    getBy(id?: number) {
+        return this.http.get<any>(`${this.provinceUrl}/${id}`)
+    }
+
     create(data: FormGroup) {
         return this.http.post(this.provinceUrl, data.value)
+    }
+
+    update(data: FormGroup, id?: number) {
+        return this.http.put(`${this.provinceUrl}/${id}`, data.value)
     }
 
     deleteBy(id: number) {
