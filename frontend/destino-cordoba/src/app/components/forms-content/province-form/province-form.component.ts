f6a0dfc135cdc0618838e4b1c25348810dd82ec0
Franco Lorca <lorcafrancog@proton.me>
2023-06-08 17:12:06 -0300

    👔 Update(province-form.component.html and ts): Implement update method with form

--- frontend/destino-cordoba/src/app/components/forms-content/province-form/province-form.component.ts
+++ frontend/destino-cordoba/src/app/components/forms-content/province-form/province-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FormGroup, Validators, FormBuilder} from "@angular/forms";
 import {ProvinceService} from "../../../services/province.service";
 import {FormInfo} from "../../../utils/FormInfo";
@@ -9,8 +9,9 @@ import {Subscription} from "rxjs";
     templateUrl: './province-form.component.html',
     styleUrls: ['./province-form.component.css']
 })
-export class ProvinceFormComponent {
+export class ProvinceFormComponent implements OnInit, OnDestroy {
 
+    getSubscription!: Subscription;
     postSubscription!: Subscription;
     updateSubscription!: Subscription;
 
@@ -22,6 +23,24 @@ export class ProvinceFormComponent {
 
     @Input() showForm: boolean = false;
     @Input() formInfo: FormInfo = {type: ''}
+
+    ngOnInit() {
+        if (this.formInfo.type.includes('update')) {
+            this.getSubscription = this.provinceService.getBy(this.formInfo.id).subscribe((res) => {
+                this.provinceForm = this.formBuilder.group({
+                    name: [res.name, [Validators.required, Validators.min(5)]],
+                    lat: [res.lat, [Validators.required,]],
+                    lon: [res.lon, [Validators.required,]],
+                })
+            })
+        }
+    }
+
+    ngOnDestroy() {
+        this.getSubscription?.unsubscribe()
+        this.postSubscription?.unsubscribe()
+        this.updateSubscription?.unsubscribe()
+    }
 
     constructor(private formBuilder: FormBuilder, private provinceService: ProvinceService) {
     }
@@ -41,6 +60,12 @@ export class ProvinceFormComponent {
     private operations() {
         if (this.formInfo.type.includes('create')) {
             this.postSubscription = this.provinceService.create(this.provinceForm).subscribe({
+                next: (res) => {
+                    this.showForm = !this.showForm;
+                }
+            })
+        } else {
+            this.updateSubscription = this.provinceService.update(this.provinceForm, this.formInfo.id).subscribe({
                 next: (res) => {
                     this.showForm = !this.showForm;
                 }
