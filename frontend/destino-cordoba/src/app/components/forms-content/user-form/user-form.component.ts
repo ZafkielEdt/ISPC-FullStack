e236205c8ef09a31c4173719768be0723882015c
Franco Lorca <lorcafrancog@proton.me>
2023-06-12 17:12:05 -0300

    👔 Update(user-form.component.ts, table-content.component.html): Add necessary changes to show user-form

--- frontend/destino-cordoba/src/app/components/forms-content/user-form/user-form.component.ts
+++ frontend/destino-cordoba/src/app/components/forms-content/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormInfo} from "../../../utils/FormInfo";
 
 @Component({
   selector: 'app-user-form',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class UserFormComponent {
 
+  @Input() formInfo: FormInfo = {id: 0, type: ''}
+  @Input() showForm: boolean = false;
 }
