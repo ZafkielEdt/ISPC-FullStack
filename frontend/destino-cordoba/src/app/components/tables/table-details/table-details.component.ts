5f2fd1e3d242bbb1f3e2f7cab30b590ed639d92e
Franco Lorca <lorcafrancog@proton.me>
2023-07-14 12:46:29 -0300

    👔 Update(table-details.component.html,.ts): Add changes for show user-form and destinations-form components

--- frontend/destino-cordoba/src/app/components/tables/table-details/table-details.component.ts
+++ frontend/destino-cordoba/src/app/components/tables/table-details/table-details.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Tables} from "../../../utils/tables";
+import {FormsService} from "../../../services/forms.service";
+import {Forms} from "../../../utils/forms";
 
 @Component({
   selector: 'app-table-details',
@@ -10,5 +12,20 @@ export class TableDetailsComponent {
 
   @Input() currentTable!: Tables;
   @Input() data: any;
+  @Output() hideTable: EventEmitter<Tables> = new EventEmitter<Tables>()
   protected readonly Tables = Tables;
+
+  constructor(private formService: FormsService) {
+  }
+
+  hidden() {
+    this.hideTable.emit(Tables.AnyTable)
+  }
+
+  showForm(action: 'update' | 'create', formType: Forms, showForm:boolean, id?: number): void {
+    this.hidden()
+    this.formService.changeForm(action, formType, showForm, id)
+  }
+
+  protected readonly Forms = Forms;
 }
